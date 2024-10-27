d4d29cd30c31e1faf50e8067c3ade61dfc5789f8
CrazyCatGames <paveldomkin@gmail.com>
2024-10-27 21:08:13 +0300

    Task 10 first and hope last version

---
+++ 3LAB/10/include/10.h
@@ -0,0 +1,28 @@
+#ifndef INC_10_10_H
+#define INC_10_10_H
+
+#include <stdio.h>
+
+typedef enum kOpt{
+	OPT_SUCCESS,
+	OPT_FAIL,
+	OPT_ERROR,
+	OPT_ERROR_MEMORY,
+	OPT_ERROR_INPUT
+} kOpt;
+
+typedef struct Node {
+	char value;
+	struct Node** children;
+	int child_count;
+	int child_capacity;
+} Node;
+
+kOpt CheckExpression(const char *expr);
+void PrintTree(Node *root, FILE *output, int x);
+void FreeTree(Node *root);
+Node* CreateNode(char value);
+Node* ParseExpression(const char** expr);
+kOpt ValidateInput(int argc, char **argv);
+
+#endif
